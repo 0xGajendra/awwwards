8e00ad677a75f610d91263c03dd6039dc953c51b
Gajendra-Rao <gajendrapawar69@gmail.com>
2025-02-27 23:52:29 +0530

    tailwind setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <h1 className='text-2xl text-violet-300 '>Hello there</h1>
+      <h1 className='text-2xl text-green-500 '>Hello there</h1>
     </div>
   )
 }
